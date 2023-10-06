73ab107184cc5db88559298a704368d8ebd1f890
rgoewedky <jagdishreddy46@gmail.com>
2023-10-06 23:19:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
