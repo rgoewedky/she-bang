aeda0f6ef08f784ee6f1435a753f02c2c4ff575b
rgoewedky <jagdishreddy46@gmail.com>
2023-10-06 23:19:00 +0530

    Add code to bring bot online

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+//load env variables
+require('dotenv').config();
+
 const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
